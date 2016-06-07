8e70ce19163698660554a45eebf7c6f057184783
ZsLinw <815750986@qq.com>
2016-06-07 16:55:11 +0800

    实验数据计算器 增加功能 剔除坏值

--- app/tools/calculator.py
+++ app/tools/calculator.py
@@ -9,17 +9,33 @@ def chauvenet(y, mean=None, stdv=None):
       stdv = y.std()
     n = len(y)
     criterion = 1.0/(2*n)
-    d = abs(y-mean)/stdv
+    d = abs(y-mean) / stdv
     d /= 2.0**0.5
     prob = erfc(d)
-    filter = prob >= criterion
-    return filter
+    filter_true = prob >= criterion
+    return filter_true
 
 
-def list_handler(y):
-    pass
-x = numpy.array([2, 4, 6, 8, 10, 12])
-y = numpy.array([3.5, 7.2, 9.5, 17.1, 20.0, 25.5])
+def str_list_to_num_list(y):
+    number_list = [float(a) if '.' in a else int(a) for a in y]
+    return number_list
 
-print y
-print chauvenet(y)
+
+def num_list_to_str_list(y):
+    str_list = [str(a) for a in y]
+    return str_list
+
+
+def get_bool_list_of_number_list(str_list):
+    num_list = str_list_to_num_list(str_list)
+    before_list = numpy.array(num_list)
+    list_bool = chauvenet(before_list)
+    get_list_bool_showed = list_bool.tolist()
+    return get_list_bool_showed
+
+if __name__ == "__main__":
+    test = ['1.2', '3', '4.56', '1.2e+10', '1547865', '-1']
+    print test
+    print str_list_to_num_list(test)
+    print chauvenet(numpy.array(str_list_to_num_list(test)))
+    print get_bool_list_of_number_list(test)
--- app/tools/views.py
+++ app/tools/views.py
@@ -8,12 +8,13 @@ from flask import redirect, render_template, request, make_response, url_for, fl
 def calculator():
     number_list_string = request.cookies.get('number_list', None)
     table_method = request.args.get('table_method', None)
+    bool_number_list_show_str = request.cookies.get('bool_number_list_show', None)
 
-    # table_method clear
-    if table_method == "clear":
-        resp = make_response(redirect(url_for('tools.calculator')))
-        resp.set_cookie('number_list', '', path=url_for('tools.calculator'), max_age=0)
-        return resp
+    # get the bool_number_list_show
+    if bool_number_list_show_str:
+        bool_number_list_show = [int(a) for a in bool_number_list_show_str.split(' ')]
+    else:
+        bool_number_list_show = None
 
     # get the number list
     if number_list_string:
@@ -21,15 +22,26 @@ def calculator():
     else:
         number_list = None
 
-    # table_method chauvenet
-    if table_method == "chauvenet":
-        from .calculator import list_handler
-        number_list, number_erase_list = list_handler(number_list)
-        # end
-        resp = make_response(redirect(url_for('tools.calculator', number_erase_list=number_erase_list)))
-        resp.set_cookie('number_list', number_list_string, path=url_for('tools.calculator'), max_age=60 * 60 * 24)
+    # table_method clear
+    if table_method == "clear":
+        resp = make_response(redirect(url_for('tools.calculator')))
+        resp.set_cookie('bool_number_list_show', '', path=url_for('tools.calculator'), max_age=0)
+        resp.set_cookie('number_list', '', path=url_for('tools.calculator'), max_age=0)
+
         return resp
 
+    # table_method chauvenet
+    if table_method == "chauvenet":
+        import calculator
+        bool_number_list_show = calculator.get_bool_list_of_number_list(number_list)
+        # end
+        resp = make_response(redirect(url_for('tools.calculator')))
+        resp.set_cookie('bool_number_list_show', ' '.join(['1' if bo else '0' for bo in bool_number_list_show]), path=url_for('tools.calculator'), max_age=60 * 3)
+        return resp
+
+    # table_method handle
+    if table_method == "handle":
+        import calculator
     # number erase
     number_erase = request.args.get('number_erase')
     if number_erase and number_list:
@@ -38,6 +50,7 @@ def calculator():
             number_list.remove(number_erase)
         number_list_string = ' '.join(number_list)
         resp = make_response(redirect(url_for('tools.calculator')))
+        resp.set_cookie('bool_number_list_show', '', path=url_for('tools.calculator'), max_age=0)
         resp.set_cookie('number_list', number_list_string, path=url_for('tools.calculator'), max_age=60 * 60 * 24)
         return resp
 
@@ -51,7 +64,8 @@ def calculator():
                               if a.isdigit() or a == '+' or a == '-' or a == 'e' or a == '.'])
             if len(number) < len(number_raw):
                 flash('请输入一个数字')
-                return render_template('tools/calculator.html', number_list=number_list)
+                render_template('tools/calculator.html', number_list=number_list,
+                                bool_number_list_show=bool_number_list_show)
 
         # list show
         if number_list and number:
@@ -64,5 +78,6 @@ def calculator():
             number_list_string = ""
         resp = make_response(redirect(url_for('tools.calculator')))
         resp.set_cookie('number_list', number_list_string, path=url_for('tools.calculator'), max_age=60 * 60 * 24)
+        resp.set_cookie('bool_number_list_show', '', path=url_for('tools.calculator'), max_age=0)
         return resp
-    return render_template('tools/calculator.html', number_list=number_list)
+    return render_template('tools/calculator.html', number_list=number_list, bool_number_list_show=bool_number_list_show)
