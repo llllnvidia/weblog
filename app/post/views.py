c18738a72652fc4cf6334a285deea26e0d586ac4
ZsLinw <815750986@qq.com>
2016-06-08 21:34:04 +0800

    debug 发博文不会保存摘要

--- app/post/views.py
+++ app/post/views.py
@@ -16,8 +16,8 @@ def txt_to_html(txt):
 
 @post.route('/article/<int:post_id>', methods=['GET', 'POST'])
 def article(post_id):
-    post = Post.query.get_or_404(post_id)
-    if not post.is_article:
+    post_show = Post.query.get_or_404(post_id)
+    if not post_show.is_article:
         abort(404)
     form = CommentForm()
     if form.validate_on_submit():
@@ -25,21 +25,21 @@ def article(post_id):
             flash("请先登录")
             return redirect(url_for('auth.login', next=str(request.url)))
         comment = Comment(body=form.body.data,
-                          post=post,
+                          post=post_show,
                           author=current_user)
-        post.author.get_message_from_admin(content=u'你收到了一条评论。', link_id=post.id, link_type='comment')
+        post_show.author.get_message_from_admin(content=u'你收到了一条评论。', link_id=post_show.id, link_type='comment')
         comment.save()
         flash('你的评论已提交。')
-        return redirect(url_for('post.article', post_id=post.id, page=-1))
+        return redirect(url_for('post.article', post_id=post_show.id, page=-1))
     page = request.args.get('page', 1, type=int)
     if page == -1:
-        page = (post.comments.count() - 1) / \
+        page = (post_show.comments.count() - 1) / \
                current_app.config['COMMENTS_PER_PAGE'] + 1
-    pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+    pagination = post_show.comments.order_by(Comment.timestamp.asc()).paginate(
         page, per_page=current_app.config['COMMENTS_PER_PAGE'],
         error_out=False)
     comments = pagination.items
-    return render_template('post/article.html', posts=[post], form=form,
+    return render_template('post/article.html', posts=[post_show], form=form,
                            comments=comments, pagination=pagination, page=page)
 
 
@@ -48,10 +48,10 @@ def article(post_id):
 def new_article():
     form = ArticleForm()
     if request.method == 'POST' and form.validate():
-        post = Post(body=form.body.data, title=form.title.data,
-                    author=current_user,
-                    summary=form.summary.data,
-                    is_article=True, category=Category.query.filter_by(id=form.category.data).first())
+        post_new = Post(body=form.body.data, title=form.title.data,
+                        author=current_user,
+                        summary=form.summary.data,
+                        is_article=True, category=Category.query.filter_by(id=form.category.data).first())
         tags = [tag.strip() for tag in form.tags.data.split(',')] if form.tags.data else None
         if tags:
             for tag in tags:
@@ -59,65 +59,65 @@ def new_article():
                 if not new_tag:
                     new_tag = Tag(tag)
                     new_tag.save()
-                post.tag(new_tag)
-        post.ping()
-        post.save()
-        return redirect(url_for('post.article', post_id=post.id, page=-1))
+                post_new.tag(new_tag)
+        post_new.ping()
+        post_new.save()
+        return redirect(url_for('post.article', post_id=post_new.id, page=-1))
     return render_template('post/edit_post.html', form=form, article=True)
 
 
 @post.route('/edit/article/<int:post_id>', methods=['GET', 'POST'])
 @login_required
 def edit_article(post_id):
-    post = Post.query.get_or_404(post_id)
-    if current_user != post.author and \
+    post_edit = Post.query.get_or_404(post_id)
+    if current_user != post_edit.author and \
             not current_user.can(Permission.MODERATE_COMMENTS):
         abort(403)
-    if post.category:
-        form = ArticleForm(category=post.category.id)
+    if post_edit.category:
+        form = ArticleForm(category=post_edit.category.id)
     else:
         form = ArticleForm()
     if request.method == 'POST' and form.validate():
-        post.title = form.title.data
-        post.body = form.body.data
-        post.summary = form.summary.data
-        post.category = Category.query.filter_by(id=form.category.data).first()
-        post.is_article = True
+        post_edit.title = form.title.data
+        post_edit.body = form.body.data
+        post_edit.summary = form.summary.data
+        post_edit.category = Category.query.filter_by(id=form.category.data).first()
+        post_edit.is_article = True
         tags = [tag.strip() for tag in form.tags.data.split(',')] if form.tags.data else None
         if tags:
-            for tag in post.tags:
+            for tag in post_edit.tags:
                 if tag.content not in tags:
-                    post.not_tag(tag)
+                    post_edit.not_tag(tag)
             for tag in tags:
                 new_tag = Tag.query.filter_by(content=tag).first()
                 if not new_tag:
                     new_tag = Tag(tag)
                     new_tag.save()
-                if new_tag not in post.tags:
-                    post.tag(new_tag)
+                if new_tag not in post_edit.tags:
+                    post_edit.tag(new_tag)
         else:
-            for tag in post.tags:
-                    post.not_tag(tag)
-        post.ping()
-        post.save()
+            for tag in post_edit.tags:
+                    post_edit.not_tag(tag)
+        post_edit.ping()
+        post_edit.save()
         flash('该文章已修改。')
         return redirect(url_for('post.article', post_id=post_id, page=-1))
-    form.title.data = post.title
-    form.summary.data = post.summary
-    form.body.data = post.body
-    form.tags.data = ','.join([str(tag.content) for tag in post.tags])
-    return render_template('post/edit_post.html', form=form, article=True, post=post)
+    form.title.data = post_edit.title
+    form.summary.data = post_edit.summary
+    form.body.data = post_edit.body
+    form.tags.data = ','.join([str(tag.content) for tag in post_edit.tags])
+    return render_template('post/edit_post.html', form=form, article=True, post=post_edit)
 
 
 @post.route('/delete-post/<int:post_id>')
 @login_required
 def delete_post(post_id):
-    post = Post.query.get_or_404(post_id)
-    if current_user != post.author and \
+    post_delete = Post.query.get_or_404(post_id)
+    if current_user != post_delete.author and \
             not current_user.can(0x0f):
         abort(403)
     else:
-        post.delete()
+        post_delete.delete()
         flash('已删除！')
         return redirect(url_for('main.neighbourhood'))
 
