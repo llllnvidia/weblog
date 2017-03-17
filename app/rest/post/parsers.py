dbbc0f37139108932ab647c7ca076b38f2eada64
linw1995 <815750986@qq.com>
2017-03-17 13:29:05 +0800

    tag parsers

--- app/rest/post/parsers.py
+++ app/rest/post/parsers.py
@@ -11,60 +11,69 @@ def category_check(value):
     raise ValueError
 
 
+def tag_parser(value):
+    tags = value.split(",")
+    is_valid = False
+    for tag in tags:
+        if tag.startswith("'") and not tag.endswith("'"):
+            break
+        if tag.startswith("\"") and not tag.endswith("'"):
+            break
+        if not tag.startswith("'") and not tag.startswith("\""):
+            break
+        if len(tag[1:-1]) == 0:
+            break
+    else:
+        is_valid = True
+    if not is_valid:
+        raise ValueError(r"""Tag sound format like 'A','B' or "A", "B" """)
+    return [tag[1:-1] for tag in tags]
+
+
 parser_post_get = reqparse.RequestParser()
+parser_post_get.add_argument("desc", type=bool, location=["args"])
+parser_post_get.add_argument("author", type=str, location=["args"])
+parser_post_get.add_argument("category", type=str, location=["args"])
 parser_post_get.add_argument(
-    "desc", type=bool, location=["json", "args", "headers"])
-parser_post_get.add_argument(
-    "author", type=str, location=["json", "args", "headers"])
-parser_post_get.add_argument(
-    "category", type=str, location=["json", "args", "headers"])
-parser_post_get.add_argument(
-    "tag",
-    dest="tags",
-    type=str,
-    action="append",
-    location=["args", "headers"])
+    "tag", dest="tags", type=tag_parser, location=["args"], help="{error_msg}")
 
 parser_post_post = reqparse.RequestParser()
 parser_post_post.add_argument(
     "title",
     type=str,
-    location=["json", "args", "headers"],
+    location=["json", "args"],
     required=True,
     help="title is required")
 parser_post_post.add_argument(
     "summary",
     type=str,
-    location=["json", "args", "headers"],
+    location=["json", "args"],
     required=True,
     help="summary is required")
 parser_post_post.add_argument(
     "body",
     type=str,
-    location=["json", "args", "headers"],
+    location=["json", "args"],
     required=True,
     help="body is required")
 parser_post_post.add_argument(
-    "category", type=category_check, location=["json"])
+    "category", type=category_check, location=["json", "args"])
 parser_post_post.add_argument(
     "tag",
     dest="tags",
-    type=str,
-    action="append",
-    location=["args", "headers"])
+    type=tag_parser,
+    location=["json", "args"],
+    help="{error_msg}")
 
 parser_post_put = reqparse.RequestParser()
+parser_post_put.add_argument("title", type=str, location=["json", "args"])
+parser_post_put.add_argument("summary", type=str, location=["json", "args"])
+parser_post_put.add_argument("body", type=str, location=["json", "args"])
 parser_post_put.add_argument(
-    "title", type=str, location=["json", "args", "headers"])
-parser_post_put.add_argument(
-    "summary", type=str, location=["json", "args", "headers"])
-parser_post_put.add_argument(
-    "body", type=str, location=["json", "args", "headers"])
-parser_post_put.add_argument(
-    "category", type=category_check, location=["json", "args", "headers"])
+    "category", type=category_check, location=["json", "args"])
 parser_post_put.add_argument(
     "tag",
     dest="tags",
-    type=str,
-    action="append",
-    location=["args", "headers"])
+    type=tag_parser,
+    location=["json", "args"],
+    help="{error_msg}")
