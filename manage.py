f8000ce28e4f07c86ab8f10d844d46547f68035b
linw1995 <815750986@qq.com>
2017-03-19 21:16:44 +0800

    add debug commad

--- manage.py
+++ manage.py
@@ -21,6 +21,9 @@ def make_shell_context():
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("runserver", Server(host='127.0.0.1', port=5000))
+manager.add_command(
+    "debug",
+    Server(host='127.0.0.1', port=5000, use_debugger=True, use_reloader=True))
 
 
 @manager.command
