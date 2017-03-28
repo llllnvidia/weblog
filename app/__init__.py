d311ac1ed534c6dd0e866e2914f70cb04fc1fa70
linw1995 <815750986@qq.com>
2017-03-28 16:32:09 +0800

    remove restApi

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api
 
 from config import config
 
 # extension
 db = SQLAlchemy()
-api = Api()
 
 
 def create_app(config_name):
@@ -20,8 +18,6 @@ def create_app(config_name):
     app.jinja_env.trim_blocks = True
 
     from .main import main as blueprint_main
-    from .rest import blueprint_api
     app.register_blueprint(blueprint_main)
-    app.register_blueprint(blueprint_api, url_prefix="/api")
 
     return app
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
-from ..rest.auth import current_user
 
 main = Blueprint('main', __name__)
 
@@ -10,4 +9,4 @@ from . import views, errors
 @main.app_context_processor
 def inject_permissions():
     menu = ["main.index"]
-    return dict(menu=menu, current_user=current_user)
+    return dict(menu=menu)
